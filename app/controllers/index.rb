5ef6e1b7a1b35fd7af06379bf4274174cbb53b13
stephenitis <nguyenhstephen@gmail.com>
2013-05-06 17:15:39 -0700

    user can search recent tweets and drag and sort them

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,15 +1,33 @@
 get '/' do
+  erb :index
 end
 
-get '/:username' do
-  @user = TwitterUser.find_by_username(params[:username])
-  if  @user.tweets.empty?
+# get '/:user_name' do
+#   @user = TwitterUser.find_by_username(params[:user_name])
+#   if  @user.tweets.empty?
+#     @user.fetch_tweets!
+#   elsif @user.tweets_stale?
+#     p "the tweets be stale mannnn"
+#     @user.fetch_tweets!
+#   end
+#   @tweets = @user.tweets.limit(10)
+# erb :_user_tweets
+# end
+
+post "/tweetfinder" do
+  p params
+  @user = TwitterUser.find_by_username(params[:user_name])
+    if  @user.tweets.empty?
     @user.fetch_tweets!
+
   elsif @user.tweets_stale?
     p "the tweets be stale mannnn"
     @user.fetch_tweets!
   end
+
   @tweets = @user.tweets.limit(10)
+
+erb :_user_tweets, :layout => false
 end
 
 #added a comment
